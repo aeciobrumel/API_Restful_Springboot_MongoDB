e3a876bc69753c80f1c3c65b65a7fc8c36b59b33
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2024-02-01 20:49:05 -0300

    endpoint para retornar os posts de um usuario

--- src/main/java/com/brumel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brumel/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.brumel.workshopmongo.domain.Post;
 import com.brumel.workshopmongo.domain.User;
 import com.brumel.workshopmongo.dto.UserDTO;
 import com.brumel.workshopmongo.services.UserService;
@@ -50,22 +51,28 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 
 	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody UserDTO objDto,@PathVariable String id) {
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 
 	}
-	
-	
+
 }
